4a93d6bb54cd989e19eb5334b79a7c02bbb0d1e5
lu4keen <victor.luchkin@gmail.com>
2026-09-18 00:42:07 +0300

    fix threads

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ app.route('/health')
 def healthcheck():
     return 'Bot is running', 200
 
-def runbot():
-    asyncio.run(start_bot())
+def runflask():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, use_reloader=False)
 
 if __name__ == '__main__':
-    bot_thread = threading.Thread(target=runbot, daemon=True)
+    bot_thread = threading.Thread(target=runflask, daemon=True)
     bot_thread.start()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    asyncio.run(start_bot())
